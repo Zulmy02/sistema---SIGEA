16fcbe70400aa039fa1d11ef06cfd37a5a2885b2
Zulmy02 <L24390228@chetumal.tecnm.mx>
2026-03-15 22:35:58 -0500

    menu hamburguesa

--- includes/navbar.php
+++ includes/navbar.php
@@ -2,7 +2,7 @@
 
 
 ?>
-<link rel="stylesheet" href="css/navbarstyle.css">
+<link rel="stylesheet" href="css/navbarstyle.css?v=<?php echo time(); ?>">
 
 <header class="main-header">
     <div class="logo-container">
@@ -16,27 +16,61 @@
             <a href="dashboard2.php">Inicio</a>
 
             <?php if ($_SESSION['rol'] == 'admin'): ?>
-                <a href="usuarios.php">Usuarios</a>
-                <a href="configuracion.php">Opciones</a>
+                <a href="">Eventos</a>
+                <a href="">Reportes</a>
+                 <a href="">Usuarios</a>
             <?php endif; ?>
 
             <?php if ($_SESSION['rol'] == 'organizador'): ?>
-                <a href="eventos.php">Mis Eventos</a>
-                <a href="reportes.php">Reportes</a>
+                <a href="">Eventos</a>
+                <a href="">Reportes</a>
+                <a href="">Configuracion</a>
+
             <?php endif; ?>
 
             <?php if ($_SESSION['rol'] == 'tomadorLista'): ?>
-                <a href="scanner.php">Pasar Lista</a>
+                <a href="">Eventos</a>
             <?php endif; ?>
 
-           <a href="logout.php" class="btn-salir" onclick="return confirmarCerrarSesion()">Cerrar sesión</a>
+            <div class="dropdown-mobile">
+                <button class="btn-mas" onclick="toggleExtraMenu(event)"> + </button>
+                <div class="dropdown-content" id="extraMenu">
+                    <?php if ($_SESSION['rol'] == 'admin'): ?>
+                        <a href="">Configuraciones</a>
+                       
+                    <?php endif; ?>
+                    
+                    
+                    <a href="logout.php" class="btn-salir-drop" onclick="return confirmarCerrarSesion()">Cerrar sesión</a>
+                </div>
+            </div>
         </nav>
     </div>
 </header>
+
 <script>
+function toggleExtraMenu(event) {
+    // Apartir de aqui  no funciona
+    event.stopPropagation(); 
+    
+    const menu = document.getElementById("extraMenu");
+    menu.classList.toggle("show");
+}
+
+// Este código detecta clics en CUALQUIER parte de la pantalla <--tampoco funciona
+window.onclick = function(event) {
+    const menu = document.getElementById("extraMenu");
+    
+    // Si el menú está abierto Y el clic NO fue dentro del menú ni en el botón...
+    if (menu.classList.contains('show')) {
+        // Verificamos que el clic no haya sido dentro del cuadrito del menú
+        if (!menu.contains(event.target) ) {
+            menu.classList.remove('show');
+        }
+    }
+}
+
 function confirmarCerrarSesion() {
-    // Esto detiene el enlace si el usuario da en "Cancelar"
-    var respuesta = confirm("¿Estás seguro de que quieres cerrar sesión?");
-    return respuesta;
+    return confirm("¿Estás seguro de que quieres cerrar sesión?");
 }
 </script>
